a3b6bb92b6e7d34258026ec42ea9c9be26224b7c
Chowewow <chowewoow@gmail.com>
2024-10-08 11:06:24 +0800

    added secret key for 'flash'

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from app.blueprints.main import main  # Import the main blueprint
 
 def create_app():
     app = Flask(__name__)
-
+    app.secret_key = 'your_secret_key'  # Replace with a secure secret key
     # Configuration settings can be added here, e.g., app.config['SECRET_KEY'] = 'your_secret_key'
 
     # Register blueprints
